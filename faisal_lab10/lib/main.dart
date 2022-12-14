a9ba5d44fb4a1a56dfffc3e2988eaba411c24ad7
FasialG <117646880+FasialG@users.noreply.github.com>
2022-12-14 23:34:58 +0300

    Creation

---
+++ faisal_lab10/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:faisal_lab10/View/WelcomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+     return GetMaterialApp(home: WelcomePage(),);
+  }
+}
